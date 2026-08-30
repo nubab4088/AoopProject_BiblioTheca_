d0dd13b2a9ac3ba885138dfc6d27e294b11538cd
nubab4088 <nbably4088@gmail.com>
2026-08-30 19:11:16 +0600

    update 3

--- springboot/demo/src/main/java/com/bibliotheca/controller/BookController.java
+++ springboot/demo/src/main/java/com/bibliotheca/controller/BookController.java
@@ -13,7 +13,6 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/books")
-@CrossOrigin(origins = "http://localhost:5173")
 public class BookController {
 
     private final BookRepository bookRepository;
--- springboot/demo/src/main/java/com/bibliotheca/controller/ChatController.java
+++ springboot/demo/src/main/java/com/bibliotheca/controller/ChatController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/chat")
-@CrossOrigin(origins = "http://localhost:5173")
+
 public class ChatController {
 
     private final GeminiService geminiService;
