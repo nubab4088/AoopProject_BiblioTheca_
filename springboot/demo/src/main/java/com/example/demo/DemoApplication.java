d6f9875b36ca591e23ecc4e3d46f0b1a07a8c70b
nubab4088 <nbably4088@gmail.com>
2026-09-01 21:57:01 +0600

    update 4

--- springboot/demo/src/main/java/com/example/demo/DemoApplication.java
+++ springboot/demo/src/main/java/com/example/demo/DemoApplication.java
@@ -2,16 +2,20 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
+
 import java.util.List;
 
 @SpringBootApplication
-// This forces Spring to actually load your PlayerController and BookController
-@ComponentScan(basePackages = {"com.example.demo", "com.bibliotheca"}) 
+@ComponentScan(basePackages = {"com.example.demo", "com.bibliotheca"})
+@EntityScan(basePackages = "com.bibliotheca.model")
+@EnableJpaRepositories(basePackages = "com.bibliotheca.repository")
 public class DemoApplication {
 
     public static void main(String[] args) {
